2f1445b117f8961a8a0343301a29f4cf2323bf48
muhammad-hassaan-y2 <mr.hassaan50@gmail.com>
2025-07-15 20:16:26 +0500

    router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
+import { projectsRouter } from '@/modules/projects/server/procedure';
 import { createTRPCRouter } from '../init';
 import { messageRouter } from '@/modules/messages/server/procedure';
 
 
 export const appRouter = createTRPCRouter({
       messages: messageRouter,
+      projects: projectsRouter,
      
 })
 export type AppRouter = typeof appRouter;
